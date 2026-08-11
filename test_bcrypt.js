238aff6505deb0c4df879334d44bf59887b469ab
Nigelec Admin <eenergietec@gmail.com>
2026-08-11 07:07:35 +0100

    chore: clean up obsolete zip archives, temporary test logs, and inactive duplicate SQLite DBs

--- test_bcrypt.js
+++
@@ -1,4 +0,0 @@
-import bcrypt from 'bcryptjs';
-
-const hash = '$2b$10$ne9fCY8MtMudJkAjv8D7QOYb.4cElivB6ursawwDH6MYbpEeJWWMoG';
-bcrypt.compare('admin123', hash).then(res => console.log('Match:', res));
